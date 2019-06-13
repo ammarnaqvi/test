93203aa15ba4f4796566cca2b2a5629cabf59081
Ammar Naqvi <git@apimatic.io>
2019-06-13 10:48:52 +0000

    sdsd

---
+++ APIMATICCalculator.Standard/Configuration.cs
@@ -0,0 +1,12 @@
+using APIMATICCalculator.Standard.Models;
+namespace APIMATICCalculator.Standard
+{
+    public partial class Configuration
+    {
+
+
+        //The base Uri for API calls
+        public static string BaseUri = "http://examples.apimatic.io/apps/calculator";
+
+    }
+}
